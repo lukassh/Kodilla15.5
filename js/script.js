95ddaa0a051b7dcce48b6fba3a9ceeae217a0d65
Lukass <lukassh@gmail.com>
2019-02-21 14:17:10 +0100

    Fix errors

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ function Button(text) {
 }
 
 Button.prototype = {
+	
 	create: function() {
 		var self = this;
-    this.element = document.createElement('button');
+    	
+    	this.element = document.createElement('button');
 		this.element.innerText = this.text;
-    this.element.addEventListener('click', function() {
-      alert(self.text);
-    });
-    document.body.appendChild(this.element);
+    	
+    	this.element.addEventListener('click', function() {
+      		alert(self.text);
+    	});
+  		document.body.appendChild(this.element);
 	}
 }
 var btn1 = new Button('Hello!');
